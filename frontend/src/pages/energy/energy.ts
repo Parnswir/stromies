7bae6de284e369c82541a460c2f7d27fa26bb5cf
Angelika Wieck <angelika.wieck@student.hpi.uni-potsdam.de>
2018-05-27 01:08:26 +0200

    frontend: energy line chart

--- frontend/src/pages/energy/energy.ts
+++ frontend/src/pages/energy/energy.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Chart } from 'chart.js';
 
 /**
  * Generated class for the EnergyPage page.
@@ -14,12 +15,48 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'energy.html',
 })
 export class EnergyPage {
+	@ViewChild('lineCanvas') lineCanvas;
+	
+    lineChart: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	constructor(public navCtrl: NavController) {
+	}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EnergyPage');
-  }
+	ionViewDidLoad() {
+
+		this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+
+            type: 'line',
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [
+                    {
+                        label: "My First dataset",
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: "rgba(75,192,192,0.4)",
+                        borderColor: "rgba(75,192,192,1)",
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: "rgba(75,192,192,1)",
+                        pointBackgroundColor: "#fff",
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                        pointHoverBorderColor: "rgba(220,220,220,1)",
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        data: [65, 59, 80, 81, 56, 55, 40],
+                        spanGaps: false,
+                    }
+                ]
+            }
+
+        });
+
+    }
 
 }
