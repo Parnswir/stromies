036873094145e946ac988c7582f58210f264ff88
Martin Michaelis <code@mgjm.de>
2018-05-27 00:59:56 +0200

    backend: added post user route

--- backend/lib/routes/index.js
+++ backend/lib/routes/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const Router = require('koa-router');
+const koaBody = require('koa-body');
 
 const router = new Router({
 	prefix: '/v1',
 });
 module.exports = router;
+
+router.bodyParser = koaBody();
 
 require('./ping');
 require('./user');
--- backend/lib/routes/user.js
+++ backend/lib/routes/user.js
@@ -2,21 +2,31 @@
 
 const {User} = require('../models');
 const router = require('.');
-const KairosClient = require('../kairos')
+const {bodyParser} = router;
+const KairosClient = require('../kairos');
 
 router.get('/user/:userID', async ctx => {
 	const {userID} = ctx.params;
-	const user = await User.getOrCreate(userID);
+	const user = await User.get(userID);
+	console.log('USER', user);
 	ctx.body = user;
 });
 
+router.post('/user/:userID', bodyParser, async ctx => {
+	const {userID} = ctx.params;
+	console.log(userID, ctx.request.body);
+	const user = await User.get(userID);
+	console.log(user);
+	await user.update(ctx.request.body).save();
+});
+
 router.get('/user/:userID/activities', async ctx => {
-  const {userID} = ctx.params;
-  const user = await User.get(userID);
+	const {userID} = ctx.params;
+	const user = await User.get(userID);
 
-  const from = ctx.query['from'] || 1483225200000
-  const to = ctx.query['to'] || 1485903600000
-  const metrics = (ctx.query['metrics'] || 'electricity').split(',')
+	const from = ctx.query.from || 1483225200000;
+	const to = ctx.query.to || 1485903600000;
+	const metrics = (ctx.query.metrics || 'electricity').split(',');
 
-  ctx.body = await new KairosClient().getActivities(userID, from, to, metrics);
+	ctx.body = await new KairosClient().getActivities(userID, from, to, metrics);
 });
