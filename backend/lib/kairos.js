c5177594becbf87806d121e23a818c60a5f1c8f6
Martin Michaelis <code@mgjm.de>
2018-05-27 01:08:56 +0200

    backend: fixed lint errors

--- backend/lib/kairos.js
+++ backend/lib/kairos.js
@@ -1,45 +1,47 @@
 'use strict';
 
-const fetch = require("node-fetch");
+/* eslint-disable camelcase */
+
+const fetch = require('node-fetch');
 
 class KairosClient {
-  constructor(url=process.env.KAIROS_URL, username=process.env.KAIROS_USER, password=process.env.KAIROS_PASSWORD) {
-    this.url = url;
-    this.headers = new fetch.Headers();
-    this.headers.append('Authorization', 'Basic ' + Buffer.from(username + ":" + password).toString('base64'));
-  }
+	constructor(url = process.env.KAIROS_URL, username = process.env.KAIROS_USER, password = process.env.KAIROS_PASSWORD) {
+		this.url = url;
+		this.headers = new fetch.Headers();
+		this.headers.append('Authorization', `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`);
+	}
 
-  async getActivities(userId, from, to, metrics) {
-    const query = {
-      "metrics": [],
-      "start_absolute": from,
-      "end_absolute": to
-    };
-    for (const metric of metrics) {
-      query["metrics"].push({
-        "tags": {
-          "user": [userId]
-        },
-        "name": "archive.consumption." + metric,
-        "aggregators": [
-          {
-            "name": "sum",
-            "sampling": {
-              "value": "1",
-              "unit": "hours"
-            }
-          }
-        ]
-      });
-    }
-    const response = await fetch(this.url, {
-      method: 'POST',
-      headers: this.headers,
-      body: JSON.stringify(query)
-    });
-    const json = await response.json();
-    return json;
-  }
+	async getActivities(userId, from, to, metrics) {
+		const query = {
+			metrics: [],
+			start_absolute: from,
+			end_absolute: to,
+		};
+		for(const metric of metrics) {
+			query.metrics.push({
+				tags: {
+					user: [userId],
+				},
+				name: `archive.consumption.${metric}`,
+				aggregators: [
+					{
+						name: 'sum',
+						sampling: {
+							value: '1',
+							unit: 'hours',
+						},
+					},
+				],
+			});
+		}
+		const response = await fetch(this.url, {
+			method: 'POST',
+			headers: this.headers,
+			body: JSON.stringify(query),
+		});
+		const json = await response.json();
+		return json;
+	}
 }
 
 module.exports = KairosClient;
--- backend/lib/routes/user.js
+++ backend/lib/routes/user.js
@@ -20,12 +20,15 @@ router.post('/user/:userID', bodyParser, async ctx => {
 	await user.update(ctx.request.body).save();
 });
 
+// 2017-01-01 to 2017-02-01
+const FROM_DEFAULT = 1483225200000;
+const TO_DEFAULT = 1483225200000;
+
 router.get('/user/:userID/activities', async ctx => {
 	const {userID} = ctx.params;
-	const user = await User.get(userID);
 
-	const from = ctx.query.from || 1483225200000;
-	const to = ctx.query.to || 1485903600000;
+	const from = ctx.query.from || FROM_DEFAULT;
+	const to = ctx.query.to || TO_DEFAULT;
 	const metrics = (ctx.query.metrics || 'electricity').split(',');
 
 	ctx.body = await new KairosClient().getActivities(userID, from, to, metrics);
