e8cd875469f3e1d2f4bb5834089da3ce5b726248
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2018-05-26 21:31:09 +0200

    home navigation added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,11 +6,21 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { GasPage } from '../pages/gas/gas';
+import {EnergyPage} from '../pages/energy/energy';
+import {ActivitiesPage} from '../pages/activities/activities';
+import {RewardsPage} from '../pages/rewards/rewards';
+import {EventsPage} from '../pages/events/events';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    GasPage,
+    EnergyPage,
+    ActivitiesPage,
+    RewardsPage,
+    EventsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +29,12 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    GasPage,
+    EnergyPage,
+    ActivitiesPage,
+    RewardsPage,
+    EventsPage
   ],
   providers: [
     StatusBar,
--- frontend/src/pages/home/home.ts
+++ frontend/src/pages/home/home.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {GasPage} from '../gas/gas';
+import {EnergyPage} from '../energy/energy';
+import {ActivitiesPage} from '../activities/activities';
+import {RewardsPage} from '../rewards/rewards';
+import {EventsPage} from '../events/events';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +12,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController) {}
+    gasPage(){
+      this.navCtrl.push(GasPage);
+    }
+    energyPage(){
+      this.navCtrl.push(EnergyPage);
+    }
+    activitiesPage(){
+      this.navCtrl.push(ActivitiesPage);
+    }
+    rewardsPage(){
+      this.navCtrl.push(RewardsPage);
+    }
+    eventsPage(){
+      this.navCtrl.push(EventsPage);
+    }
   }
-
-}
